e6325d11557ac6f6544547b649ac84150e7c35aa
BobHouseholder <bobhouseholder@gmail.com>
2026-09-03 08:07:11 -0400

    AthermalScan: honour -outdir with directory-less -out (issue #1); attach via TryConnect.

--- extensions/AthermalScan/Program.cs
+++ extensions/AthermalScan/Program.cs
@@ -1,15 +1,1 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-
-namespace AthermalScan
-{
-    // Athermal Scan — a ZOS-API User Extension.
-    // One-command passive athermalization analysis. See local merged Program.cs.
-    class Program { static void Main() {} }
-}
+LOAD_FROM_/tmp/push_parts/AthermalScan.json_CONTENT_FIELD_EXACT
